bfed70f518bde62c0af90005a5126b226d71fa61
bingoo <bingoo.huang@gmail.com>
2021-01-18 23:51:45 +0800

    Update custom_test.go

--- jso/custom/custom_test.go
+++ jso/custom/custom_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -57,13 +58,32 @@ func (p Person) MarshalJSON() ([]byte, error) {
 	// This alias will have all the same fields, but none of the methods (MarshalJSON/UnmarshalJSON).
 	type Alias Person
 
-	return json.Marshal(struct {
+	v := struct {
 		Name string `json:"name"`
-		*Alias
+		Alias
 	}{
-		Alias: (*Alias)(&p),
+		Alias: Alias(p),
 		Name:  "Fixed:" + p.Name,
-	})
+	}
+
+	printStructMeta(p)
+	printStructMeta(&p)
+	printStructMeta(v.Alias)
+	printStructMeta(&v.Alias)
+
+	// Person Field:(0)(Name)
+	// Person Field:(1)(Age)
+	// Person Method:(0)(MarshalJSON)
+	// *Person Field:(0)(Name)
+	// *Person Field:(1)(Age)
+	// *Person Method:(0)(MarshalJSON)
+	// *Person Method:(1)(UnmarshalJSON)
+	// Alias Field:(0)(Name)
+	// Alias Field:(1)(Age)
+	// *Alias Field:(0)(Name)
+	// *Alias Field:(1)(Age)
+
+	return json.Marshal(v)
 }
 
 func (p *Person) UnmarshalJSON(data []byte) error {
@@ -103,6 +123,33 @@ func TestCustom2(t *testing.T) {
 	var d2 Document
 	assert.Nil(t, json.Unmarshal(v2, &d2))
 	assert.Equal(t, d, d2)
+}
+
+func printStructMeta(i interface{}) {
+	v, ok := i.(reflect.Value)
+	if !ok {
+		v = reflect.ValueOf(i)
+	}
+
+	typ := v.Type().Name()
+	fv := v
+
+	if v.Kind() == reflect.Ptr {
+		fv = v.Elem()
+		typ = "*" + fv.Type().Name()
+	}
+
+	if fv.Kind() != reflect.Struct {
+		return
+	}
+
+	for i := 0; i < fv.NumField(); i++ {
+		fmt.Printf("%s Field:(%d)(%s)\n", typ, i, fv.Type().Field(i).Name)
+	}
+
+	for i := 0; i < v.NumMethod(); i++ {
+		fmt.Printf("%s Method:(%d)(%s)\n", typ, i, v.Type().Method(i).Name)
+	}
 }
 
 func TestCustom(t *testing.T) {
